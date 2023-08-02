8a8be53a4cdf95eaafe7b68ce353f223ed206ba5
Pedro Fernandes <thephfernandes@gmail.com>
2023-08-02 16:20:09 -0300

    separa endpoints de oi e tchau

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-	res.send("hello world");
+	res.send("Nao tem nada por aqui, tente dar oi ou tchau!")
+});
+
+app.get("/oi", (req, res) => {
+	res.send("Hello world!");
+});
+
+app.get("/tchau", (req, res) => {
+	res.send("Goodbye earth!");
 });
 
 app.listen(port, () => {
