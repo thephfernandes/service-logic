d8925272c22c3a2d383015d56b6bc33e67ce8298
pedro fernandes <thephfernandes@gmail.com>
2023-08-07 22:06:46 -0300

    update food route

--- routes/food.js
+++ routes/food.js
@@ -1,25 +1,84 @@
 const router = require('express').Router();
 
-router.get("/macas", (req, res) => {
-	res.send("Estamos sem maças no momento, tente mais tarde");
+let inventory = [
+	{
+		id: 1,
+		product: "apple",
+		quantity: 10
+	},
+	{
+		id: 2,
+		product: "banana",
+		quantity: 15
+	},
+	{
+		id: 3,
+		product: "orange",
+		quantity: 20
+	},
+	{
+		id: 4,
+		product: "grapes",
+		quantity: 8
+	},
+	{
+		id: 5,
+		product: "watermelon",
+		quantity: 5
+	},
+	{
+		id: 6,
+		product: "strawberries",
+		quantity: 12
+	}
+];
+
+
+router.get("/inventory", (req, res) => {
+	res.json(inventory);
 });
 
-router.get("/pizza", (req, res) => {
-	res.json(
-		[
-			{
-				"id": 1,
-				"nome": "calabresa"
-			},
-			{
-				"id": 2,
-				"nome": "marguerita"
-			},
-			{
-				"id": 3,
-				"nome": "brigadeiro"
-			}
-		])
+router.get("/inventory/:id", (req, res) => {
+	const itemId = parseInt(req.params.id);
+	const item = inventory.find(item => item.id === itemId);
+
+	if (item) {
+		res.json(item);
+	} else {
+		res.status(404).json({ message: "Item not found" });
+	}
+});
+
+router.post("/inventory", (req, res) => {
+	const newItem = req.body;
+	newItem.id = inventory.length + 1;
+	inventory.push(newItem);
+	res.status(201).json(newItem);
+});
+
+router.put("/inventory/:id", (req, res) => {
+	const itemId = parseInt(req.params.id);
+	const updatedItem = req.body;
+	const index = inventory.findIndex(item => item.id === itemId);
+
+	if (index !== -1) {
+		inventory[index] = { ...inventory[index], ...updatedItem };
+		res.json(inventory[index]);
+	} else {
+		res.status(404).json({ message: "Item not found" });
+	}
+});
+
+router.delete("/inventory/:id", (req, res) => {
+	const itemId = parseInt(req.params.id);
+	const index = inventory.findIndex(item => item.id === itemId);
+
+	if (index !== -1) {
+		const deletedItem = inventory.splice(index, 1);
+		res.json(deletedItem[0]);
+	} else {
+		res.status(404).json({ message: "Item not found" });
+	}
 });
 
 module.exports = router;
